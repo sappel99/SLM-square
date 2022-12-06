195d5db8bbd1b3039dafacd0b3d1ca0ac7bdcda6
sappel99 <simon.appel99@gmail.com>
2022-12-06 11:30:47 +0100

    Make view #5

--- square/src/main/java/at/fhtw/appel/square/Controller/SquareController.java
+++ square/src/main/java/at/fhtw/appel/square/Controller/SquareController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+@Controller
 public class SquareController {
     private final SquareService squareService;
 
@@ -14,10 +14,21 @@ public class SquareController {
     }
 
 
-    /*@GetMapping("/square/{square}")
-    String getSqu(@PathVariable int square, Model model) {
+    @GetMapping("/home")
+    public String home() {
+        return "home";  //home.html
+    }
+
+    @GetMapping("/square")
+    String getSqu(Model model) {
+        model.addAttribute("square", squareService.getSquare()); //set the message for mes.html
+
+        return "erg";   //mes.html
+    }
+    @GetMapping("/square/{square}")
+    String setSqu(@PathVariable int square, Model model) {
         squareService.setSquare(square);
         model.addAttribute("square",squareService.getSquare());   //set the text for change.html
         return "erg";    //erg.html
-    }*/
+    }
 }
