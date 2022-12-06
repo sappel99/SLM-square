0844fa5916bf246b656a2e1ba83255b5f01ab22e
sappel99 <simon.appel99@gmail.com>
2022-12-06 11:41:05 +0100

    Write Tests #7

--- square/src/test/java/at/fhtw/appel/square/SquareApplicationTests.java
+++ square/src/test/java/at/fhtw/appel/square/SquareApplicationTests.java
@@ -1,5 +1,6 @@
 package at.fhtw.appel.square;
 
+import at.fhtw.appel.square.Controller.SquareController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
